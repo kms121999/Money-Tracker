b63204bd6d99084c856828fdb00b5e9d59cfa39a
kms121999 <smithkeaton12@gmail.com>
2021-11-19 16:45:24 -0600

    Create Account.py

---
+++ app/modules/classes/Account.py
@@ -0,0 +1,13 @@
+import decimal
+
+
+
+class Account:
+  def __init__(self, account_name, account_properties):
+    """ Account
+    str account_name - display name of the account
+    ??? account_properties - ###!!! what data type is account properties?
+    """
+    
+    self.name = account_name
+    self.properties = account_properties ###!!!
