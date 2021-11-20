558016b2ff420ee54a39922b359a41e9b930e75f
kms121999 <smithkeaton12@gmail.com>
2021-11-19 19:05:54 -0600

    Update Account.py

--- app/modules/classes/Account.py
+++ app/modules/classes/Account.py
@@ -1,6 +1,8 @@
-import decimal
+from decimal import Decimal
 
-
+def test_decimal(x):
+  if type(x) != Decimal:
+      raise TypeError("Expected Decimal type.")
 
 class Account:
   def __init__(self, account_name, account_properties):
@@ -11,3 +13,18 @@ class Account:
     
     self.name = account_name
     self.properties = account_properties ###!!!
+    
+    self.balance = Decimal("0.00")
+    
+    self.transactions = set()
+
+    
+  def __add__(self, amount):
+    test_decimal(amount)
+    self.balance += amount
+    return True
+    
+  def __sub__(self, amount):
+    test_decimal(amount)
+    self.balance -= amount
+    return True
